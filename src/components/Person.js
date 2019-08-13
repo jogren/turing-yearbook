75f04707bfb162ba9a2aa7ff51a2136d05f3b469
Jacob Ogren <jogren0044@gmail.com>
2019-08-13 15:35:07 -0600

    Add content editable to all cards both student and staff

--- src/components/Person.js
+++ src/components/Person.js
@@ -5,10 +5,10 @@ const Person = ({id, photo, name, quote, superlative, deleteMember}) => {
   return (
     <section className="section-person">
       <img className="section-img"src={photo} />
-      <h3 className="section-h3">{name}</h3>
-      <h4 className="section-h4">"{quote}"</h4>
-      <h5 className="section-h5">{superlative}</h5>
-      <button onClick={() => { deleteMember(id) }}>Delete</button>
+      <h3 className="section-h3" contentEditable="true" suppressContentEditableWarning={true}>{name}</h3>
+      <h4 className="section-h4" contentEditable="true" suppressContentEditableWarning={true}>"{quote}"</h4>
+      <h5 className="section-h5" contentEditable="true" suppressContentEditableWarning={true}>{superlative}</h5>
+      <button className="Person-button" onClick={() => { deleteMember(id) }}>Delete</button>
     </section>
   )
 }
